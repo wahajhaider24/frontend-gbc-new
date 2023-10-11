c43fc5f7f86a44bad35947f4e0266491e94b9b99
wahajhaider24 <wahajhaider@live.com>
2023-10-10 21:04:54 -0400

    lab3

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,27 +1,90 @@
 import React from "react";
+import "./Todolist.css"
 
-const TodoList = () =>{
+class TodoList extends React.Component{
 
+
+    constructor(){
+        super(); 
+        this.state ={
+            todos: [],
+            newTodo: ''
+        };
+    }
+
+    addTodo= (task) =>{
+            this.setState((prev)=>({
+                todos:[...prev.todos, task],
+                newTodo: ''
+            }));
+    }
+
+    handleChange =(e)=>{
+        console.log(e.target.value,"<<<")
+        this.setState({newTodo: e.target.value})
+    }
+
+    addList= () =>{
+           if(this.state.newTodo.trim()===null)
+            return
+            
+            this.addTodo(this.state.newTodo);
+           
+            
+    }
+
+    removeList=(index)=>{
+        const updatedArry=[...this.state.todos];
+        updatedArry.splice(index,1);
+        this.setState({
+            todos: updatedArry
+        })
+    }
+    render(){
     return(<>
-        <div>
+        <div className="todoListBox">
             <h1 style={{textAlign:'center', color:'black'}}>My To Do List</h1>
             <div style={{textAlign:"center"}}>
             <input
                 type="text"
                 placeholder="Enter Your Task"
                 style={{padding:"10px"}}
+                value={this.state.newTodo}
+                onChange={this.handleChange}
+
             >
             </input>
             <button
-            style={{backgroundColor:"darkblue", border:"1px", color:"white", padding:"10px", 
-            }}>
+            style={{backgroundColor:"darkblue", border:"1px", color:"white", height:'40px' 
+            }}
+            onClick={this.addList}
+            
+            >
+
                 Add
             </button>
-            <ul style={{listStyleType:"circle"}}></ul>
+            <div className="list">
+            <ul style={{listStyleType:"circle"}}>
+               { console.log(this.state.todos,"<<<")}
+                   { this.state.todos.map((todo,index)=>{
+                        return(
+                        <li key={index}>
+                            {todo}
+                            <button className="removeButton" onClick={()=>{this.removeList(index)}}>Remove</button>
+                        </li>
+                        );
+
+                    })
+
+                }
+                
+            </ul>
+            </div>
             </div>
             </div>
        
     </>)
+    }
 }
 
 export default TodoList;
